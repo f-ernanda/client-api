b0c561134a767665434216e201c804d85f407bec
Fernanda <f_ernanda@outlook.com.br>
2022-10-16 19:31:55 -0300

    Add audit data

--- src/main/java/com/fernanda/client/entities/Client.java
+++ src/main/java/com/fernanda/client/entities/Client.java
@@ -17,6 +17,10 @@ public class Client implements Serializable {
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant birthDate;
     private Integer children;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Client() {
     }
@@ -77,6 +81,18 @@ public class Client implements Serializable {
     public void setChildren(Integer children) {
         this.children = children;
     }
+
+    public Instant getCreatedAt() { return createdAt; }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() { createdAt = Instant.now(); }
+
+    @PreUpdate
+    public void preUpdate() { updatedAt = Instant.now(); }
 
     @Override
     public boolean equals(Object o) {
