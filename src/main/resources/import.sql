14f153620622ee23a1fe182de6ce9094d01347c6
Fernanda <f_ernanda@outlook.com.br>
2022-10-16 17:48:04 -0300

    Add database seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Carlos Souza', '12345678901', 3100.0, TIMESTAMP WITH TIME ZONE '1990-08-13T03:51:00Z', 0);
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Maria Aparecida Santos', '23456789012', 4050.0, TIMESTAMP WITH TIME ZONE '1970-02-04T12:27:00Z', 1);
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Osmar Rodriguez da Silva', '34567890123', 1500.0, TIMESTAMP WITH TIME ZONE '1965-12-02T17:45:00Z', 2);
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Ana Albrecht Lima', '45678901234', 7000.0, TIMESTAMP WITH TIME ZONE '1998-04-19T02:07:00Z', 0);
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Yasmin Oliveira', '56789012345', 5400.0, TIMESTAMP WITH TIME ZONE '1966-06-15T22:30:00Z', 3);
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Victor de Sousa ', '67890123456', 2400.0, TIMESTAMP WITH TIME ZONE '1971-01-01T10:00:00Z', 1);
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Emmanuel Pereira', '78901234567', 2000.0, TIMESTAMP WITH TIME ZONE '1960-05-11T014:25:00Z', 2);
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Domingo Neves Silva', '89012345678', 6200.0, TIMESTAMP WITH TIME ZONE '1983-09-16T16:42:00Z', 3);
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Anderson Augusto dos Anjos', '90123456789', 3950.0, TIMESTAMP WITH TIME ZONE '1979-10-24T05:23:00Z', 1);
+INSERT INTO client (name, cpf, income, birth_date, children) VALUES ('Meg Silva', '01234567890', 8500.0, TIMESTAMP WITH TIME ZONE '1988-11-29T06:30:00Z', 0);
